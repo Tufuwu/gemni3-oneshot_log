23bd445a34bbb41b342086f56ff57934e12c5c11
wu <wu@.com>
2026-01-29 17:49:58 +0800

    Logs-apache/airavata-django-portal

--- setup.py
+++ setup.py
@@ -1,34 +1,56 @@
-from setuptools import setup
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
 
 setup(
-    name='neat-python',
-    version='1.0.0',
-    author='Alan Mcintyre, Cesar Gomes Miguel, Carolina Feher da Silva, Marcio Lobo Netto',
-    author_email='alan@codereclaimers.com',
-    maintainer='Alan McIntyre',
-    maintainer_email='alan@codereclaimers.com',
-    url='https://github.com/CodeReclaimers/neat-python',
-    license="BSD",
-    description='A NEAT (NeuroEvolution of Augmenting Topologies) implementation',
-    long_description='Python implementation of NEAT (NeuroEvolution of Augmenting Topologies), a method ' +
-                     'developed by Kenneth O. Stanley for evolving arbitrary neural networks. ' +
-                     'Version 1.0.0 includes full innovation number tracking as described in the original NEAT paper.',
-    long_description_content_type= 'text/x-rst',
-    packages=['neat', 'neat/iznn', 'neat/nn', 'neat/ctrnn'],
+    name='airavata-django-portal',
+    version='0.1',
+    url='https://github.com/apache/airavata-django-portal',
+    author='Apache Software Foundation',
+    author_email='dev@airavata.apache.org',
+    description=('The Airavata Django Portal is a web interface to the '
+                 'Apache Airavata API implemented using the Django web '
+                 'framework.'),
+    long_description=read('README.md'),
+    license='Apache License 2.0',
+    packages=find_packages(),
+    install_requires=[
+            'Django',
+            'djangorestframework',
+            'requests',
+            'requests-oauthlib',
+            'thrift',
+            'thrift_connector',
+            'wagtail',
+            'wagtailfontawesome',
+            'jupyter',
+            'papermill',
+            "airavata-django-portal-sdk",
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'flake8-isort'
+        ],
+        'mysql': [
+            'mysqlclient'
+        ]
+    },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Scientific/Engineering'
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application'
     ]
 )
